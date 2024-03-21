732c38fff1c21f9e16e88cf36117175a1a422224
Aryan Bansal <91452049+Bansal29@users.noreply.github.com>
2024-03-21 15:42:52 +0530

    env added

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const connectToMongo = require("./db");
 const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
-const port = 5000;
 
 app.use(express.json());
 var cors = require("cors");
@@ -16,6 +15,10 @@ connectToMongo();
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-app.listen(port, () => {
+const port = process.env.PORT || 7000;
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error in Running Serve ${port}: ` + err);
+  }
   console.log(`iNoteBook notes app listening on port ${port}`);
 });
